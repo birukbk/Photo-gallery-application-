d07ae5f35c1da4eec520e050245f9a5f8615b232
Biruk G <birukbk@gmail.com>
2016-03-30 16:28:53 +0100

    adds file directory, install and uploads. install contains sql data for deployment upload folder is where the uploaded images will be saved.

---
+++ install/photogallery.sql
@@ -0,0 +1,8 @@
+CREATE TABLE photographgallery (
+id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+filename varchar (255) NOT null,
+type varchar(100) not null,
+size int (11) not null,
+description text not null,
+title varchar(255) not null
+);
--- upload.php
+++ upload.php
@@ -1,61 +1,59 @@
+<?php 
+echo "<pre>";
+print_r($_FILES['userfile']);
+
+echo "<pre>";
+
+ ?>
+
 <?php
-/**
- * artist Page
- */
 
-/*Include the application configuration settings
-Include the database class definition*/
-require_once 'includes/initialize.php';
+// In an application, this could be moved to a config file
+$upload_errors = array(
+	// http://www.php.net/manual/en/features.file-upload.errors.php
+		UPLOAD_ERR_OK 				=> "No errors.",
+		UPLOAD_ERR_INI_SIZE  	=> "Larger than upload_max_filesize.",
+		UPLOAD_ERR_FORM_SIZE 	=> "Larger than form MAX_FILE_SIZE.",
+		UPLOAD_ERR_PARTIAL 		=> "Partial upload.",
+		UPLOAD_ERR_NO_FILE 		=> "No file.",
+		UPLOAD_ERR_NO_TMP_DIR => "No temporary directory.",
+		UPLOAD_ERR_CANT_WRITE => "Can't write to disk.",
+		UPLOAD_ERR_EXTENSION 	=> "File upload stopped by extension."
+		);
 
- 
-
-
-
-$output = '';
-
-    
-    // check if there were any results
+if(isset($_POST['submit'])) {
+	// process the form data
+	$tmp_file = $_FILES['userfile']['tmp_name'];
+	$target_file = basename($_FILES['userfile']['name']);
+	$upload_dir = "uploads";
   
-        
-        //html output with table
-		
-		$output .='';
-	 
-	//get the values from database
-		// 
-			
-		// Close table tag	
-	 
+	// You will probably want to first use file_exists() to make sure
+	// there isn't already a file by the same name.
 	
-    
-
-
-
-// Close the connection
-
-
-
-$content='';
-$tpl_head=file_get_contents('includes/head.html');
-$tpl = file_get_contents('templates/uploadForm.html');
-$page_footer=file_get_contents('includes/footer.html');
-
-$title = '';
-$heading = 'Upload image';
-$artist_page_content= $output;
-
-//parse a template and populate it with values
-$page_header=parseTemplate($tpl_head,array('{{ page_heading }}' => $heading));
-$artistPageContent=parseTemplate($tpl, array( 
-                                    '{{ page_title }}' => $title ,
-                                    '{{ page_heading }}' => $heading,
-                                    '{{ content }}' => $artist_page_content));
-
-//constracting the artist page
-$content.=$page_header;
-$content.=$artistPageContent;
-$content.=$page_footer;
-
-echo $content;
+	// move_uploaded_file will return false if $tmp_file is not a valid upload file 
+	// or if it cannot be moved for any other reason
+	if(move_uploaded_file($tmp_file, $upload_dir."/".$target_file)) {
+		$message = "File uploaded successfully.";
+	} else {
+		$error = $_FILES['userfile']['error'];
+		$message = $upload_errors[$error];
+	}
+	
+}	
 
 ?>
+
+ <div id="UploadForm">
+             <?php if(!empty($message)){echo "<p>{$message}</p>";} ?>
+             <form  action="upload.php" enctype="multipart/form-data"  method="POST">
+                <div>
+                 <input name="userfile" type="file" id="fileinput" />
+                 
+                 <p>Title:<input type="text" name="title" value="" size="37"> </p> 
+          
+                 <p>Description:</p><textarea name="description" rows="5" cols="40"></textarea></br> 
+            
+                 <input type="submit" name="submit" value="Upload"/>
+                </div>
+            </form>
+             </div>
