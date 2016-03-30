a0f92ebcff7b06ded2ad0e833afcd367efbf5f70
Biruk G <birukbk@gmail.com>
2016-03-30 18:45:53 +0100

    Defines DS as directory separator.

--- includes/initialize.php
+++ includes/initialize.php
@@ -4,7 +4,7 @@ require_once("config.php");
 require_once("database.php");
 require_once("functions.php");
 require_once("photograph.php");
-
+defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
 
 
  ?>
