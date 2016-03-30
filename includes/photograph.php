a3db269500cfd40ab8f83f3d2b394409f9900bb8
Biruk G <birukbk@gmail.com>
2016-03-30 19:02:07 +0100

    adds save method to photograph class.

--- includes/photograph.php
+++ includes/photograph.php
@@ -45,6 +45,52 @@ class Photograph extends database{
 
 		}
 	}
+		public function save() {
+		if(isset($this->id)) {
+			$this->update();
+		} else {
+			// Make sure there are no errors
+			
+			// Can't save if there are pre-existing errors
+		  if(!empty($this->errors)) { return false; }
+		  
+			// Make sure the caption is not too long for the DB
+		  if(strlen($this->caption) > 255) {
+				$this->errors[] = "The caption can only be 255 characters long.";
+				return false;
+			}
+		
+		  // Can't save without filename and temp location
+		  if(empty($this->filename) || empty($this->temp_path)) {
+		    $this->errors[] = "The file location was not available.";
+		    return false;
+		  }
+			
+			// Determine the target_path
+		  $target_path = SITE_ROOT .DS. 'public' .DS. $this->upload_dir .DS. $this->filename;
+		  
+		  // Make sure a file doesn't already exist in the target location
+		  if(file_exists($target_path)) {
+		    $this->errors[] = "The file {$this->filename} already exists.";
+		    return false;
+		  }
+		
+			// Attempt to move the file 
+			if(move_uploaded_file($this->temp_path, $target_path)) {
+		  	// Success
+				// Save a corresponding entry to the database
+				if($this->create()) {
+					// We are done with temp_path, the file isn't there anymore
+					unset($this->temp_path);
+					return true;
+				}
+			} else {
+				// File was not moved.
+		    $this->errors[] = "The file upload failed, possibly due to incorrect permissions on the upload folder.";
+		    return false;
+			}
+		}
+	}
 
 
 
