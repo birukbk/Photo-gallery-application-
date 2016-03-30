d15675cb7e0cb6bbe401160d232efc980da8109e
Biruk G <birukbk@gmail.com>
2016-03-30 17:13:00 +0100

    adds photograph class.

---
+++ includes/photograph.php
@@ -0,0 +1,53 @@
+<?php 
+require_once('includes/intialize.php');
+class Photograph extends database{
+	protected static $table_name="photographgallery";
+	protected static $db_fields=array('id', 'filename', 'type', 'size', 'description','title');
+	public $id;
+	public $filename;
+	public $type;
+	public $size;
+	public $description;
+	public $title;
+	
+	private $temp_path;
+    protected $upload_dir="images";
+    public $errors=array();
+
+    protected $upload_errors = array(
+		// http://www.php.net/manual/en/features.file-upload.errors.php
+		UPLOAD_ERR_OK 				=> "No errors.",
+		UPLOAD_ERR_INI_SIZE  	=> "Larger than upload_max_filesize.",
+	  UPLOAD_ERR_FORM_SIZE 	=> "Larger than form MAX_FILE_SIZE.",
+	  UPLOAD_ERR_PARTIAL 		=> "Partial upload.",
+	  UPLOAD_ERR_NO_FILE 		=> "No file.",
+	  UPLOAD_ERR_NO_TMP_DIR => "No temporary directory.",
+	  UPLOAD_ERR_CANT_WRITE => "Can't write to disk.",
+	  UPLOAD_ERR_EXTENSION 	=> "File upload stopped by extension."
+	);
+
+	// Pass in $_FILE(['uploaded_file']) as an argument
+  public function attach_file($file) {
+		// error checking on the form parameters
+		if(!$file || empty($file) || !is_array($file)) {
+		  
+		  $this->errors[] = "No file was uploaded.";
+		  return false;
+		} elseif($file['error'] != 0) {
+		  $this->errors[] = $this->upload_errors[$file['error']];
+		  return false;
+		} else {
+		  $this->temp_path  = $file['tmp_name'];
+		  $this->filename   = basename($file['name']);
+		  $this->type       = $file['type'];
+		  $this->size       = $file['size'];
+			return true;
+
+		}
+	}
+
+
+
+}
+
+ ?>
