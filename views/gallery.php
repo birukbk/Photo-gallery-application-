eb0f3400e4480e7661884584d3b2f1e228432f6a
Biruk G <birukbk@gmail.com>
2016-04-03 10:31:17 +0100

    rearranges page.

---
+++ views/gallery.php
@@ -0,0 +1,41 @@
+<?php 
+include(dirname(dirname(__FILE__))."/includes/head.html");
+include(dirname(dirname(__FILE__))."/includes/initialize.php");
+
+$photos=photograph::find_all();
+// echo "<br/>";
+// echo  dirname(__FILE__);
+// echo "<br/>";
+// echo dirname(dirname(__FILE__));
+?>
+
+ 
+<?php  
+
+
+        $output ='';
+	
+		foreach ($photos as $photo) :
+			
+	    	$output.='<p>';
+	    	$output.='<br/>';
+	    	$output.='<a href="'.$photo->image_path().'">';
+	    	$output.='<img src="'.$photo->image_path2().'">';
+	    	$output.='<br/>';
+	    	$output.=$photo->title;
+	    	$output.='</p>';
+	 		
+	    endforeach;
+	 	echo $output;
+
+
+	 		
+
+
+?>
+	
+
+
+
+
+
