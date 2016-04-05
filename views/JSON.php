933eb89d7533a2104e3c05633fa123e9adb6cddf
Biruk G <birukbk@gmail.com>
2016-04-05 19:58:46 +0100

    minor edits to JSON.php

--- views/JSON.php
+++ views/JSON.php
@@ -2,10 +2,14 @@
 include(dirname(dirname(__FILE__))."/includes/initialize.php");
 $id=$_GET['id'];
 $photo=photograph::find_by_id($id);
-
-list($width, $height, $type, $attr) = getimagesize('../'.$photo->image_path());
+if ($photo) {
+  list($width, $height, $type, $attr) = getimagesize('../'.$photo->image_path());
 $result=json_encode(array("Title"=>$photo->title, "Description"=>$photo->description, "Filename"=>$photo->filename, "width"=>$width,"hight"=>$height));
 echo $result;
+}else if ($photo===false) {
+   echo "The requested id doesn't exist!";
+   die;
+}
 ?>
 
 
